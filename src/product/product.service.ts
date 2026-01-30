3c5f9532e522fcf16e55f42241b0d4bde061753a
lucasNasc <lucasfortunato328@gmail.com>
2026-01-30 20:34:32 -0300

    correcoes no metodo create with recipe

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -69,7 +69,7 @@ export class ProductService {
     await queryRunner.connect();
     await queryRunner.startTransaction();
 
-    const recipe = [];
+    const productIngredientData: ProductIngredient[] = [];
 
     try {
       const doesEmployeeReallyExists = await queryRunner.manager.findOne(
@@ -84,6 +84,19 @@ export class ProductService {
       if (!doesEmployeeReallyExists) {
         throw new UnauthorizedException('Funcionário não encontrado');
       }
+
+      const data = {
+        name: createProductWithRecipe.name,
+        category: createProductWithRecipe.category,
+        unities: createProductWithRecipe.unities,
+        expirationDate: createProductWithRecipe.expirationDate,
+        lowStock: createProductWithRecipe.lowStock,
+        price: createProductWithRecipe.price,
+      };
+
+      const createProduct = queryRunner.manager.create(Product, data);
+
+      const newProduct = await queryRunner.manager.save(Product, createProduct);
 
       for (const supply of createProductWithRecipe.productIngredient) {
         const doesSupplyReallyExists = await queryRunner.manager.findOne(
@@ -101,44 +114,135 @@ export class ProductService {
           );
         }
 
-        const productIngredientData = {
+        const productIngredientDataLoop = {
           supplyRealTime: doesSupplyReallyExists,
+          product: newProduct,
           employee: doesEmployeeReallyExists,
           quantity: supply.quantity,
         };
 
         const createProductIngredient = queryRunner.manager.create(
           ProductIngredient,
-          productIngredientData,
+          productIngredientDataLoop,
         );
 
-        const newProductIngredient = await queryRunner.manager.save(
-          ProductIngredient,
-          createProductIngredient,
-        );
+        productIngredientData.push(createProductIngredient);
+      }
 
-        if (!createProductIngredient || !newProductIngredient) {
-          throw new InternalServerErrorException(
-            `Erro ao cadastrar ingrediente ${doesSupplyReallyExists.name} do produto ${createProductIngredient.product}`,
-          );
-        }
+      const newRecipe = await queryRunner.manager.save(
+        ProductIngredient,
+        productIngredientData,
+      );
 
-        recipe.push(newProductIngredient);
+      await queryRunner.commitTransaction();
+
+      return {
+        product: newProduct,
+        recipe: newRecipe,
+      };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+
+      this.logger.error(`Erro ao cadastrar produto: ${error.message}`);
+
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        'Falha ao processar transação na criação de produto com receita',
+      );
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async CreateWithRecipeAndRegisteredSupplies(
+    tokenPayloadDTO: TokenPayloadDTO,
+    createProductWithRecipe: CreateProductWithRecipeDTO,
+  ) {
+    const findEmployee = await this.employeesService.FindById(
+      tokenPayloadDTO.sub,
+    );
+
+    if (!findEmployee) {
+      throw new UnauthorizedException('Funcionário não encontrado');
+    }
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const productIngredientData: ProductIngredient[] = [];
+
+    try {
+      const doesEmployeeReallyExists = await queryRunner.manager.findOne(
+        Employee,
+        {
+          where: {
+            id: tokenPayloadDTO.sub,
+          },
+        },
+      );
+
+      if (!doesEmployeeReallyExists) {
+        throw new UnauthorizedException('Funcionário não encontrado');
       }
 
       const data = {
-        ...createProductWithRecipe,
-        recipe: recipe,
+        name: createProductWithRecipe.name,
+        category: createProductWithRecipe.category,
+        unities: createProductWithRecipe.unities,
+        expirationDate: createProductWithRecipe.expirationDate,
+        lowStock: createProductWithRecipe.lowStock,
+        price: createProductWithRecipe.price,
       };
 
       const createProduct = queryRunner.manager.create(Product, data);
 
       const newProduct = await queryRunner.manager.save(Product, createProduct);
 
+      for (const supply of createProductWithRecipe.productIngredient) {
+        const doesSupplyReallyExists = await queryRunner.manager.findOne(
+          SupplyRealTime,
+          {
+            where: {
+              id: supply.supplyId,
+            },
+          },
+        );
+
+        if (!doesSupplyReallyExists) {
+          throw new UnauthorizedException(
+            `Insumo ${supply.supplyId} da receita do produto ${createProductWithRecipe.name} não encontrado`,
+          );
+        }
+
+        const productIngredientDataLoop = {
+          supplyRealTime: doesSupplyReallyExists,
+          product: newProduct,
+          employee: doesEmployeeReallyExists,
+          quantity: supply.quantity,
+        };
+
+        const createProductIngredient = queryRunner.manager.create(
+          ProductIngredient,
+          productIngredientDataLoop,
+        );
+
+        productIngredientData.push(createProductIngredient);
+      }
+
+      const newRecipe = await queryRunner.manager.save(
+        ProductIngredient,
+        productIngredientData,
+      );
+
       await queryRunner.commitTransaction();
 
       return {
-        ...newProduct,
+        product: newProduct,
+        recipe: newRecipe,
       };
     } catch (error) {
       await queryRunner.rollbackTransaction();
