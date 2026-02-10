e337068eb48e414b8a11c419c12d2e8fa06895b5
lucasNasc <lucasfortunato328@gmail.com>
2026-02-10 13:36:01 -0300

    adicionando rota de products

---
+++ src/product/product.controller.ts
@@ -0,0 +1,130 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { SetRoutePolicy } from 'src/auth/decorators/set-route-policy.decorator';
+import { TokenPayloadDTO } from 'src/auth/dto/token-payload.dto';
+import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
+import { UrlUuidDTO } from 'src/common/dto/url-uuid.dto';
+import { EmployeeRole } from 'src/common/enums/employee-role.enum';
+import { CreateProductWithRecipeDTO } from './dto/create-product-with-recipe.dto';
+import { CreateProductWithoutRecipeDTO } from './dto/create-product-without-recipe.dto';
+import { PaginationByCategoryDTO } from './dto/pagination-category.dto';
+import { PaginationByExpDateDTO } from './dto/pagination-exp-date.dto';
+import { PaginationByNameDTO } from './dto/pagination-name.dto';
+import { PaginationByPriceDTO } from './dto/pagination-price.dto';
+import { PaginationByUnitiesDTO } from './dto/pagination-unities.dto';
+import { UpdateProductDTO } from './dto/update-product.dto';
+import { ProductFindService } from './product-find.service';
+import { ProductService } from './product.service';
+
+@Controller('products')
+export class ProductController {
+  constructor(
+    private readonly productService: ProductService,
+    private readonly productFindService: ProductFindService,
+  ) {}
+
+  @Post('create/withoutRecipe')
+  @SetRoutePolicy(EmployeeRole.CREATE)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  CreateWithoutRecipe(
+    @TokenPayloadParam() tokenPayloadDTO: TokenPayloadDTO,
+    @Body() body: CreateProductWithoutRecipeDTO,
+  ) {
+    return this.productService.CreateWithoutRecipe(tokenPayloadDTO, body);
+  }
+
+  @Post('create/withRecipe')
+  @SetRoutePolicy(EmployeeRole.CREATE)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  CreateWithRecipe(
+    @TokenPayloadParam() tokenPayloadDTO: TokenPayloadDTO,
+    @Body() body: CreateProductWithRecipeDTO,
+  ) {
+    return this.productService.CreateWithRecipe(tokenPayloadDTO, body);
+  }
+
+  @Post('create/withRecipe/registeredSupplies')
+  @SetRoutePolicy(EmployeeRole.CREATE)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  CreateWithRecipeAndRegisteredSupplies(
+    @TokenPayloadParam() tokenPayloadDTO: TokenPayloadDTO,
+    @Body() body: CreateProductWithRecipeDTO,
+  ) {
+    return this.productService.CreateWithRecipeAndRegisteredSupplies(
+      tokenPayloadDTO,
+      body,
+    );
+  }
+
+  @Post('create/recipe')
+  @SetRoutePolicy(EmployeeRole.CREATE)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  CreateRecipe(
+    @TokenPayloadParam() tokenPayloadDTO: TokenPayloadDTO,
+    @Body() body: any,
+  ) {
+    const { productId, createRecipe, useStockSupplies } = body;
+
+    return this.productService.CreateRecipe(
+      tokenPayloadDTO,
+      productId,
+      createRecipe,
+      useStockSupplies,
+    );
+  }
+
+  @Patch()
+  @SetRoutePolicy(EmployeeRole.UPDATE)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  UpdateProduct(
+    @TokenPayloadParam() tokenPayloadDTO: TokenPayloadDTO,
+    @Param('id') id: UrlUuidDTO,
+    @Body() updateProductDTO: UpdateProductDTO,
+  ) {
+    return this.productService.Update(tokenPayloadDTO, id, updateProductDTO);
+  }
+
+  @Get('search/name/')
+  @SetRoutePolicy(EmployeeRole.READ)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  FindByName(@Query() paginationByNameDto: PaginationByNameDTO) {
+    return this.productFindService.FindByName(paginationByNameDto);
+  }
+
+  @Get('search/category/')
+  @SetRoutePolicy(EmployeeRole.READ)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  FindByCategory(@Query() paginationByCategoryDto: PaginationByCategoryDTO) {
+    return this.productFindService.FindByCategory(paginationByCategoryDto);
+  }
+
+  @Get('search/price/')
+  @SetRoutePolicy(EmployeeRole.READ)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  FindByPrice(@Query() paginationByPriceDto: PaginationByPriceDTO) {
+    return this.productFindService.FindByPrice(paginationByPriceDto);
+  }
+
+  @Get('search/unities/')
+  @SetRoutePolicy(EmployeeRole.READ)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  FindByUnities(@Query() paginationByUnitiesDto: PaginationByUnitiesDTO) {
+    return this.productFindService.FindByUnities(paginationByUnitiesDto);
+  }
+
+  @Get('search/expirationDate/')
+  @SetRoutePolicy(EmployeeRole.READ)
+  @SetRoutePolicy(EmployeeRole.PRODUCTS)
+  FindByExpirationDate(
+    @Query() paginationByExpDateDto: PaginationByExpDateDTO,
+  ) {
+    return this.productFindService.FindByExpirationDate(paginationByExpDateDto);
+  }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeModule } from 'src/employee/employee.module';
 import { Product } from './entities/product.entity';
 import { ProductFindService } from './product-find.service';
+import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product]), EmployeeModule],
+  controllers: [ProductController],
   providers: [ProductService, ProductFindService],
 })
 export class ProductModule {}
