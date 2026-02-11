d90a992cc00e6f203a1ed286dbcbe6019b378938
lucasNasc <lucasfortunato328@gmail.com>
2026-02-11 14:02:04 -0300

    adicionando modulos no app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigType } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { EmployeeModule } from 'src/employee/employee.module';
+import { JWTBlacklistModule } from 'src/jwt-blacklist/jwt-blacklist.module';
+import { LogsModule } from 'src/logs-register/log.module';
 import { OutflowModule } from 'src/outflow/outflow.module';
 import { ProductModule } from 'src/product/product.module';
+import { RefreshTokensModule } from 'src/refresh-tokens/refresh-token.module';
 import { SaleModule } from 'src/sale/sale.module';
 import { SupplyModule } from 'src/supply/suppy.module';
 import appConfig from './app.config';
@@ -35,8 +41,18 @@ import { AppService } from './app.service';
     OutflowModule,
     SaleModule,
     ProductModule,
+    AuthModule,
+    JWTBlacklistModule,
+    LogsModule,
+    RefreshTokensModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
