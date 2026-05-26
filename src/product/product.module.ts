1d2d0141c8b16dbae15ba78e3a3fa931ddeb9876
lucasNasc <lucasfortunato328@gmail.com>
2026-05-26 17:58:10 -0300

    correcao em product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeModule } from 'src/employee/employee.module';
 import { RoleModule } from 'src/role/role.module';
 import { SupplyRealTime } from 'src/supply/entities/supply-realtime.entity';
+import { ProductInflow } from './entities/product-inflow.entity';
 import { ProductIngredient } from './entities/product-ingredient.entity';
 import { Product } from './entities/product.entity';
 import { ProductFindService } from './product-find.service';
@@ -11,7 +12,12 @@ import { ProductService } from './product.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product, SupplyRealTime, ProductIngredient]),
+    TypeOrmModule.forFeature([
+      Product,
+      SupplyRealTime,
+      ProductIngredient,
+      ProductInflow,
+    ]),
     EmployeeModule,
     RoleModule,
   ],
